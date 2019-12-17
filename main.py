290487ff193ee85b8b6f6f074a9b13b242b8f5cb
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2019-12-17 13:48:16 +0100

    Adding requirements files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #configuration for pywikibot
 import os
+import sys
 import pywikibot
 from pywikibot import config2
 family = 'my'
@@ -9,7 +10,7 @@ if not os.path.isfile(familyfile):
   print ("family file %s is missing" % (familyfile))
 config2.register_family_file(family, familyfile)
 config2.password_file = "user-password.py"
-config2.usernames['my']['my'] = 'Admin'
+config2.usernames['my']['my'] = 'Max'
 
 #connect to the wikibase
 wikibase = pywikibot.Site("my", "my")
@@ -20,21 +21,30 @@ wikidata = pywikibot.Site("wikidata", "wikidata")
 wikidata_repo = wikidata.data_repository()
 
 #import an item
-from util.util import changeItem
-# wikidata_item = pywikibot.ItemPage(wikidata_repo, 'Q27')
-# wikidata_item.get()
-# changeItem(wikidata_item, wikibase_repo, True)
+from util.util import changeItem, changeProperty, importProperty
 
-# import a list
+# item = sys.argv[1]
+# wikidata_item = pywikibot.PropertyPage(wikidata_repo, item)
+# wikidata_item.get()
+# print(wikidata_item.getID())
+# importProperty(wikidata_item)
+
+#import a list
 filepath = 'list'
 with open(filepath) as fp:
     line = fp.readline()
     while line:
         print("Importing " + line.replace("\n", ""))
         if not line.startswith("#"):
-            wikidata_item = pywikibot.ItemPage(wikidata_repo, line)
-            wikidata_item.get()
-            changeItem(wikidata_item, wikibase_repo, True)
+            if (line.startswith("Q")):
+                wikidata_item = pywikibot.ItemPage(wikidata_repo, line)
+                wikidata_item.get()
+                changeItem(wikidata_item, wikibase_repo, True)
+            elif (line.startswith("P")):
+                wikidata_property = pywikibot.PropertyPage(wikidata_repo, line)
+                wikidata_property.get()
+                changeItem(wikidata_item, wikibase_repo, True)
+
         line = fp.readline()
 
 # check if there are changes
