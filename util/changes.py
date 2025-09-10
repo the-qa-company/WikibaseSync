71191713fe65b7422705e79eae0de63cd8695891
J.M <jonathan.mallet3872@gmail.com>
2025-09-10 17:26:43 +0200

    Adding UserAgent to respect Wikidata new policy

--- util/changes.py
+++ util/changes.py
@@ -5,6 +5,10 @@ from datetime import datetime, timedelta,timezone
 def recent_changes(rccontinue, minutes, url ="https://wikidata.org/w/api.php"):
     time = datetime.now(timezone.utc) - timedelta(minutes=minutes)
     S = requests.Session()
+    headers = {
+        "User-Agent": "WikibaseSync/1.0 (https://github.com/the-qa-company/WikibaseSync/)"
+    }
+    S.headers.update(headers)
 
     parameters = {
         "format": "json",
